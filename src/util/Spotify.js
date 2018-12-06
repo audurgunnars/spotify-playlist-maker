6210f46eec96ed7d845f694d6345d54707978681
Auður Gunnarsdóttir <audurgunnarsdottir@Auurs-MacBook-Pro.local>
2018-12-06 14:39:30 +0100

    refactor: savePlaylist API calls

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -4,16 +4,62 @@ const scope = 'playlist-modify-private playlist-modify-public'
 const url = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&scope=${scope}`
 let accessToken = ''
 let expiresIn = 0
+
+const getUserID = async accessToken => {
+  try {
+    const response = await fetch('https://api.spotify.com/v1/me', {
+      headers: { Authorization: `Bearer ${accessToken}`}
+    })
+    if (response.ok) {
+      const jsonResponse = await response.json()
+      return jsonResponse.id
+    }
+  } catch (error) {
+    return console.log(error)
+  }
+}
+const createPlaylistID = async (userID, accessToken, playlistName) => {
+  try {
+    const playlistResponse = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+      headers: {Authorization: `Bearer ${accessToken}`},
+      method: 'POST',
+      body: JSON.stringify({name: playlistName})
+    })
+    if (playlistResponse.ok) {
+      const jsonPlaylistResponse = await playlistResponse.json()
+      return jsonPlaylistResponse.id
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+const trackToPlaylist = async (playlistID, accessToken, trackURIs) => {
+  try {
+    const addSongsToPlaylist = await fetch(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {
+      headers: {Authorization: `Bearer ${accessToken}`},
+      method: 'POST',
+      body: JSON.stringify({uris: trackURIs})
+    })
+    return addSongsToPlaylist
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const Spotify = {
   _getAccessToken: () => {
     if (accessToken) return accessToken
     const currentUrl = window.location.href
-    accessToken = currentUrl.match(/access_token=([^&]*)/)[1]
-    expiresIn = +currentUrl.match(/expires_in=([^&]*)/)[1]
+    accessToken = currentUrl.match(/access_token=([^&]*)/)
+    expiresIn = currentUrl.match(/expires_in=([^&]*)/)
+
     if (accessToken && expiresIn) {
+      accessToken = accessToken[1]
+      expiresIn = +expiresIn[1]
+
       window.setTimeout(() => accessToken = '', expiresIn * 1000)
       window.history.pushState('Access Token', null, '/')
-      return
+      return accessToken
     }
     window.location = url
   },
@@ -33,41 +79,9 @@ const Spotify = {
   },
   savePlaylist: async (playlistName, trackURIs) => {
     const accessToken = Spotify._getAccessToken()
-    let userID = ''
-    let playlistID = ''
-    try {
-      const response = await fetch('https://api.spotify.com/v1/me', {
-        headers: { Authorization: `Bearer ${accessToken}`}
-      })
-      if (response.ok) {
-        const jsonResponse = await response.json()
-        userID = jsonResponse.id
-      }
-    } catch (error) {
-      return console.log(error)
-    }
-    try {
-      const playlistResponse = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
-        headers: {Authorization: `Bearer ${accessToken}`},
-        method: 'POST',
-        body: JSON.stringify({name: playlistName})
-      })
-      if (playlistResponse.ok) {
-        const jsonPlaylistResponse = await playlistResponse.json()
-        playlistID = jsonPlaylistResponse.id
-      }
-    } catch (error) {
-      console.log(error)
-    }
-    try {
-      const addSongsToPlaylist = await fetch(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {
-        headers: {Authorization: `Bearer ${accessToken}`},
-        method: 'POST',
-        body: JSON.stringify({uris: trackURIs})
-      })
-    } catch (error) {
-      console.log(error)
-    }
+    const userID = await getUserID(accessToken)
+    const playlistID = await createPlaylistID(userID, accessToken)
+    await trackToPlaylist(playlistID, accessToken, trackURIs)
   }
 }
 
