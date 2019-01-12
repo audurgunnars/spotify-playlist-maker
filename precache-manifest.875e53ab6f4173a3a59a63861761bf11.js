752f1acd708df9cc7f89a19cd764555c13696eeb
Auður Gunnarsdóttir <audurgunnarsdottir@Auurs-MacBook-Pro.local>
2019-01-12 17:05:40 +0100

    Updates

---
+++ precache-manifest.875e53ab6f4173a3a59a63861761bf11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "53ccb5121c849fce0dc6",
+    "url": "/jammming/static/js/runtime~main.53ccb512.js"
+  },
+  {
+    "revision": "84e9204b6528cb4dfd13",
+    "url": "/jammming/static/js/main.84e9204b.chunk.js"
+  },
+  {
+    "revision": "21dbb108c0b6ed034f52",
+    "url": "/jammming/static/js/1.21dbb108.chunk.js"
+  },
+  {
+    "revision": "84e9204b6528cb4dfd13",
+    "url": "/jammming/static/css/main.ae959a1f.chunk.css"
+  },
+  {
+    "revision": "cd3cd7104c859ca7ac283e1a73972fb7",
+    "url": "/jammming/index.html"
+  }
+];
